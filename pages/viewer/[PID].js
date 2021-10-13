ef9cb9fa2a1ea15124bad106c6bdd2d986174481
Roman Paska <paska1993roman@gmail.com>
2021-10-13 21:13:35 +0300

    Added loader to Viewer page

--- pages/viewer/[PID].js
+++ pages/viewer/[PID].js
@@ -4,8 +4,12 @@ import { useRouter } from "next/router";
 import { Container } from "react-bootstrap";
 import Navbar from "../../src/Components/Navbar";
 import Meta from "../../src/Components/ArticleHeaders";
-
 import PayWall from "../../src/Components/Viewer/PayWall";
+
+// Disabled SSR 𐂂
+const Loader = dynamic(() => import("../../src/Components/Loader"), {
+  ssr: false
+});
 
 function Viewer() {
   const router = useRouter();
@@ -18,15 +22,18 @@ function Viewer() {
 
   return (
     <>
-      <Meta
-        title={data.metadata.article_title}
-        description={data.metadata.article_description}
-        url={`${process.env.NEXT_PUBLIC_API_URL}/articles/` + PID}
-      />
-      <Container>
-        <Navbar />
-        <PayWall />
-      </Container>
+      {!data && <Loader Title="Loading Viewer" Description="Please wait" />}
+      {data && (
+        <Meta
+          title={data.metadata.article_title}
+          description={data.metadata.article_description}
+          url={`${process.env.NEXT_PUBLIC_API_URL}/articles/` + PID}
+        />
+        <Container>
+          <Navbar />
+          <PayWall />
+        </Container>
+      )}
     </>
   );
 }
