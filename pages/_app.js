612dd53eb4b7ae84ae54fada66185858476e2811
Roman Paska <paska1993roman@gmail.com>
2021-10-13 22:06:07 +0300

    debug

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
 import axios from "axios";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import "../pages/styles.css";
 
@@ -12,9 +13,12 @@ function MyApp({ Component, pageProps }) {
       <SWRConfig
         value={{
           refreshInterval: 10000,
-          fetcher: (url) => axios.get(url).then((res) => res.data),
+          fetcher: url => axios.get(url).then(res => res.data)
         }}
       >
+        <Head>
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
         <Toaster />
         <Component {...pageProps} />
       </SWRConfig>
