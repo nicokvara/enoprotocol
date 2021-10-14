cce89cb49457b7ed3b994cee91104b600653d421
Roman Paska <paska1993roman@gmail.com>
2021-10-14 08:25:39 +0300

    Solana url changes

--- src/Functions/Pay.js
+++ src/Functions/Pay.js
@@ -4,13 +4,13 @@ import {
   PublicKey,
   Transaction,
   clusterApiUrl,
-  SystemProgram,
+  SystemProgram
 } from "@solana/web3.js";
 
 async function Pay(receiver, amount) {
   try {
     // Connect to Solana
-    const network = "https://api.mainnet-beta.solana.com";
+    const network = "https://api.devnet.solana.com";
     const connection = new Connection(network);
 
     // A new transaction
@@ -18,16 +18,14 @@ async function Pay(receiver, amount) {
       SystemProgram.transfer({
         fromPubkey: window.solana.publicKey,
         toPubkey: receiver,
-        lamports: amount,
+        lamports: amount
       })
     );
     // Fee Payer
     transaction.feePayer = window.solana.publicKey;
 
     // Blockhash
-    transaction.recentBlockhash = (
-      await connection.getRecentBlockhash()
-    ).blockhash;
+    transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
 
     // Signature
     const signedTransaction = await window.solana.signTransaction(transaction);
