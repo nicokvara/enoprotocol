a0e67dbf30838c2752f6a9661e598a8ca37a1adf
ShellRox <georgechakhnashvili@gmail.com>
2022-06-16 20:00:35 +0400

    step-by-step verbosity
    obviously, you could replace `console.assert` with custom UI based alerts.

--- src/Functions/Pay.js
+++ src/Functions/Pay.js
@@ -26,12 +26,24 @@ async function Pay(receiver, amount) {
 
     // Blockhash
     transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+    console.assert(transaction.recentBlockhash.length > 0, "Transaction failure: could not gather blockhash for transaction")
+    
+    
+    
 
     // Signature
-    const signedTransaction = await window.solana.signTransaction(transaction);
-    const signature = await connection.sendRawTransaction(
-      signedTransaction.serialize()
-    );
+    try {
+      const signedTransaction = await window.solana.signTransaction(transaction);
+    } catch (error) {
+      console.assert("Transaction failure: transaction could not be signed")
+    }
+    try {
+      const signature = await connection.sendRawTransaction(
+        signedTransaction.serialize()
+      );
+    } catch (error) {
+      console.assert("Transaction failure: transaction could not be confirmed");
+    }
   } catch (error) {
     console.log(error);
     if (error) {
