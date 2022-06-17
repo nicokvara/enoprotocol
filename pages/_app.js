db15eebc733ce5ad91abee3dc8ae853512767b0e
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2022-06-17 22:20:17 +0300

    fix meta

--- pages/_app.js
+++ pages/_app.js
@@ -30,10 +30,10 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="Eno.xyz is a censorship resistant interface for creating and interacting with protected content powered by the distributed ledger and cryptographic technologies. Eno.xyz doesn't collect any data from users allowing them to stay fully anonymous if they so desire. Currently only textual content is supported."
         />
-        <link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
+        {/* <link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} /> */}
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
+        {/* <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} /> */}
         <meta
           property="og:title"
           content="Eno - Create, own and monetize your content"
