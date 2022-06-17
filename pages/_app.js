fbdfcbeb89b989ced83eef574835513ed6e37189
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2022-06-17 22:05:28 +0300

    fix meta

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
         />
         <meta
           name="description"
-          content="1231Eno.xyz is a censorship resistant interface for creating and interacting with protected content powered by the distributed ledger and cryptographic technologies. Eno.xyz doesn't collect any data from users allowing them to stay fully anonymous if they so desire. Currently only textual content is supported."
+          content="Eno.xyz is a censorship resistant interface for creating and interacting with protected content powered by the distributed ledger and cryptographic technologies. Eno.xyz doesn't collect any data from users allowing them to stay fully anonymous if they so desire. Currently only textual content is supported."
         />
         <link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
 
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
         <meta
           property="og:title"
-          content="123Eno - Create, own and monetize your content"
+          content="Eno - Create, own and monetize your content"
         />
         <meta
           property="og:image"
@@ -50,7 +50,7 @@ function MyApp({ Component, pageProps }) {
         />
         <meta
           property="twitter:title"
-          content="132Eno - Create, own and monetize your content"
+          content="Eno - Create, own and monetize your content"
         />
         <meta
           property="twitter:image"
